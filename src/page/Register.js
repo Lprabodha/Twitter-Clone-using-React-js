d8ec70cd4a42d2e660a8542449c1ef65665e2e7a
lahiru prabodha <67253461+Lprabodha@users.noreply.github.com>
2021-05-31 15:05:11 +0530

    Particles add
    Particles add

--- src/page/Register.js
+++ src/page/Register.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import Particles from 'react-particles-js';
 
 const useStyles = makeStyles((theme) => ({
  
 root:{
   height:"100vh",
-  background:"#16202C",
+  
   display:'flex',
   justifyContent:'center',
   alignItems:'center',
@@ -59,6 +60,14 @@ title:{
 desc:{
   color:'#fff',
   fontSize:'1.3rem',
+},
+canvas:{
+  position:'absolute',
+  width:'100%',
+  height:'100%',
+  zIndex:'-1',
+  background:"#16202C",
+
 }
 
 
@@ -103,6 +112,29 @@ export default function  Register() {
     </div>
 
 <img src={process.env.PUBLIC_URL+ '/assets/creative-team.svg'} alt="" width="500px"/>
+
+<Particles
+className={classes.canvas}
+params={{
+  particles:{
+    number:{
+      value:50,
+    },
+    size:{
+     value:2, 
+    },
+    shape:{
+      type:['circle'],
+    },
+  },
+}
+
+}
+
+/>
+
+
+
     </div>
   )
 }
